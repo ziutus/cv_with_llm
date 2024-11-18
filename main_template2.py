55dc121ebb1a9bf87d6618a99d15bc0f297754cb
Krzysztof Jozwiak <krzysztof.jozwiak@linuxexpert.pl>
2024-11-18 17:09:03 +0100

    Rename variables for consistency in `create_cv` function
    Renamed several variables to improve consistency and clarity in the `create_cv` function and its associated functions. Ensured all related functions use the renamed variables to maintain uniformity throughout the code.

--- main_template2.py
+++ main_template2.py
@@ -93,30 +93,30 @@ def draw_entry_right(c, text, visual_setup, y_position, page_number):
     return y_position, page_number
 
 
-def create_cv(filename, cv_data_json, visual_config):
+def create_cv(filename, cv_data_json, visual_config_cv):
     c = canvas.Canvas(filename, pagesize=A4)
 
-    pdfmetrics.registerFont(TTFont(visual_config['fonts']['default'], 'fonts/ARIAL.TTF'))
-    pdfmetrics.registerFont(TTFont(visual_config['fonts']['bold'], 'fonts/ARIALBD.TTF'))
-    pdfmetrics.registerFont(TTFont(visual_config['fonts']['bold-italic'], 'fonts/ARIALBLACKITALIC.TTF'))
-    pdfmetrics.registerFont(TTFont(visual_config['fonts']['italic'], 'fonts/ArialCEItalic.ttf'))
-    c.setFont(visual_config['fonts']['default'], visual_config['sizes']['normal'])
+    pdfmetrics.registerFont(TTFont(visual_config_cv['fonts']['default'], 'fonts/ARIAL.TTF'))
+    pdfmetrics.registerFont(TTFont(visual_config_cv['fonts']['bold'], 'fonts/ARIALBD.TTF'))
+    pdfmetrics.registerFont(TTFont(visual_config_cv['fonts']['bold-italic'], 'fonts/ARIALBLACKITALIC.TTF'))
+    pdfmetrics.registerFont(TTFont(visual_config_cv['fonts']['italic'], 'fonts/ArialCEItalic.ttf'))
+    c.setFont(visual_config_cv['fonts']['default'], visual_config_cv['sizes']['normal'])
 
     page_number = 1
 
-    c.setFillColor(HexColor(visual_config['colors']['text']))
-    draw_left_column(c, cv_data_json, height, page_number, visual_config)
-    draw_right_column(c, cv_data_json, height, page_number, visual_config)
+    c.setFillColor(HexColor(visual_config_cv['colors']['text']))
+    draw_left_column(c, cv_data_json, height, page_number, visual_config_cv)
+    draw_right_column(c, cv_data_json, height, page_number, visual_config_cv)
 
     c.save()
 
 
-def draw_left_column_empty(c, height, visual_config_left):
+def draw_left_column_empty(c, high, visual_config_left):
 
     c.setFillColor(HexColor(visual_config_left['colors']['grey_background']))
-    c.rect(y_left_column_grey_top, 50, column_left_width, height - 100, stroke=0, fill=1)
+    c.rect(x_left_column_grey_top, y_left_column_grey_bottom, column_left_width, high - column_left_high_minus, stroke=0, fill=1)
 
-def draw_left_column(c, cv_data_json, height, page_number, visual_config_left):
+def draw_left_column(c, cv_data_json, high, page_number, visual_config_left):
 
     personal_info = cv_data_json.get("personal_info", {})
     top_skills = cv_data_json.get("top_skills", [])
@@ -130,12 +130,12 @@ def draw_left_column(c, cv_data_json, height, page_number, visual_config_left):
 
 
     c.setFillColor(HexColor(visual_config_left['colors']['grey_background']))
-    c.rect(y_left_column_grey_top, y_left_column_grey_bottom, column_left_width, height - 40, stroke=0, fill=1)
+    c.rect(x_left_column_grey_top, y_left_column_grey_bottom, column_left_width, high - column_left_high_minus, stroke=0, fill=1)
 
     if page_number == 1:
         c.setFillColor(HexColor(visual_config_left['colors']['highlight']))
 
-        y_position = height - 40
+        y_position = high - y_left_top_text_margin
 
         y_position, page_number = draw_entry_left(c, f"{personal_info['name']}", visual_config_left['left_name_surname'], y_position, page_number)
         y_position, page_number = draw_entry_left(c, f"Email: {personal_info['email']}", visual_config_left['left_email_phone'], y_position, page_number)
@@ -201,12 +201,12 @@ def draw_left_column(c, cv_data_json, height, page_number, visual_config_left):
 
 
 def draw_personal_data_info(c, personal_data, visual_config_section, page_number):
-    visual_config = visual_config_section['personal_data_info']
+    visual_config_personal = visual_config_section['personal_data_info']
 
-    c.setFont(visual_config['font'], visual_config['font_size'])
-    c.setFillColor(HexColor(visual_config['color']))
+    c.setFont(visual_config_personal['font'], visual_config_personal['font_size'])
+    c.setFillColor(HexColor(visual_config_personal['color']))
 
-    draw_entry_left(c, personal_data, visual_config, visual_config['y_position'], page_number)
+    draw_entry_left(c, personal_data, visual_config_personal, visual_config_personal['y_position'], page_number)
 
 def draw_courses_left(c, course, y_position, visual_setup, page_number):
     c.setFont(visual_config['fonts']['default'], visual_config['sizes']['normal'])
@@ -224,25 +224,25 @@ def draw_education_entry_left(c, edu, y_position, visual_setup, page_number):
 
     return y_position, page_number
 
-def draw_right_column(c, cv_data_json, height, page_number, visual_config):
+def draw_right_column(c, cv_data_json, high, page_number, visual_config_right):
 
     experience = cv_data_json.get("experience", [])
     personal_data_info = cv_data_json.get("personal_data_info", "")
 
-    y_position = height - y_right_column_text_top_margin
+    y_position = high - y_right_column_text_top_margin
 
-    y_position, page_number = draw_entry_right(c, "Work Experience", visual_config['section_name'],
+    y_position, page_number = draw_entry_right(c, "Work Experience", visual_config_right['section_name'],
                                                y_position, page_number)
 
     #
-    y_position -= visual_config['right_own_project']['Y_top_margin']
+    y_position -= visual_config_right['right_own_project']['Y_top_margin']
     for job in experience:
         y_position, page_number = draw_experience_entry(c, job, y_position, page_number)
-        y_position -= visual_config['right_own_project']['Y_margin']
+        y_position -= visual_config_right['right_own_project']['Y_margin']
 
-    draw_right_column_projects(c, cv_data_json, y_position, page_number, visual_config)
+    draw_right_column_projects(c, cv_data_json, y_position, page_number, visual_config_right)
 
-    draw_personal_data_info(c, personal_data_info, visual_config, page_number)
+    draw_personal_data_info(c, personal_data_info, visual_config_right, page_number)
 
     # add_footer(c, page_number, visual_config)
 
@@ -276,24 +276,24 @@ def draw_experience_entry(c, job, y_position, page_number):
 
 
 
-def draw_right_column_projects(c, cv_data_json, y_position, page_number, visual_config):
+def draw_right_column_projects(c, cv_data_json, y_position, page_number, visual_config_projects):
     own_projects = cv_data_json.get("own_projects", [])
 
-    visual_config_default = visual_config['right_own_project']['default']
-    visual_config_link = visual_config['right_own_project']['link']
+    visual_config_default = visual_config_projects['right_own_project']['default']
+    visual_config_link = visual_config_projects['right_own_project']['link']
 
     if own_projects['position'] == "right":
-        y_position -= visual_config['right_own_project']['Y_delta']
-        y_position, page_number = draw_entry_right(c, f"Own projects", visual_config['section_name'], y_position, page_number)
+        y_position -= visual_config_projects['right_own_project']['Y_delta']
+        y_position, page_number = draw_entry_right(c, f"Own projects", visual_config_projects['section_name'], y_position, page_number)
         for project in own_projects["projects"]:
             y_position, page_number = draw_entry_right_with_superscript(c, project["name"], project["time"], visual_config_default, y_position, page_number)
 
             y_position, page_number = draw_entry_right(c, project["link_to_show"], visual_config_link, y_position,  page_number)
             if "technologies" in project:
                 technologies = "Technologies: " + ", ".join(project["technologies"])
-                y_position, page_number = draw_entry_right(c, technologies, visual_config['experience']['technologies'],
+                y_position, page_number = draw_entry_right(c, technologies, visual_config_projects['experience']['technologies'],
                                                            y_position, page_number)
-            y_position -= visual_config['right_own_project']['Y_delta']
+            y_position -= visual_config_projects['right_own_project']['Y_delta']
 
 
 if __name__ == '__main__':
@@ -308,8 +308,10 @@ if __name__ == '__main__':
 
     # y_left_top_margin = visual_config['y_left_top_margin']
     y_top_margin = visual_config['y_top_margin'] #For new page but we have 1 page CV
-    y_left_column_grey_top = visual_config['y_left_column_grey_top']
+    y_left_top_text_margin = visual_config['y_left_top_text_margin']
+    x_left_column_grey_top = visual_config['x_left_column_grey_top']
     column_left_width = visual_config['column_left_width']
+    column_left_high_minus = visual_config['column_left_high_minus']
     y_left_column_text_min = visual_config['y_left_column_text_min']
     y_left_column_text_max = y_left_column_text_min + column_left_width - visual_config['y_left_text_right_margin']
     y_left_column_grey_bottom = visual_config['y_left_column_grey_bottom']
